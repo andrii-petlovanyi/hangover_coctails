2563b8d5274559969340033f70539cb890ee662d
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-08 18:24:38 +0200

    remove code to index.js

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,20 +1,23 @@
-import { getRandCoctList } from './js/api';
-import { searchCoctById } from './js/components/modal';
-
+import { getRandCoctList, getCoctById } from './js/api';
+import sprite from './images/svg/sprite.svg';
 async function getListCard() {
   try {
     const { data } = await getRandCoctList();
-    // const cocktailsList = lengthCocktailList(data.drinks);
-    renderMarkup(data.drinks);
+    const cocktailsList = lengthCocktailList(data.drinks);
+    renderMarkup(cocktailsList);
   } catch (error) {}
 }
 
 function lengthCocktailList(arr = []) {
-  if (document.body.clientWidth <= 767.9) {
+  if (document.body.clientWidth <= 767) {
     return arr.splice(0, 3);
-  } else if (document.body.clientWidth > 768) {
+  } else if (
+    document.body.clientWidth > 768 &&
+    document.body.clientWidth < 1279
+  ) {
     return arr.splice(0, 6);
   } else if (document.body.clientWidth > 1280) {
+    console.log(document.body.clientWidth);
     return arr.splice(0, 9);
   }
 }
@@ -27,10 +30,10 @@ function renderMarkup(data = []) {
       <h3 class="coctail-card__name">${strDrink}</h3>
       <div class="coctail-card__options">
         <button class="button-learn_more" data-id=${idDrink} data-type="learn">Learn more</button>
-        <button class="button-add_to">
+        <button class="button-add_to" data-id=${idDrink} data-add="add">
           Add to
           <svg class="heart-icon" width="18" height="18">
-            <use href="./images/svg/sprite.svg#icon-Heart"></use>
+            <use href="${sprite}#icon-Heart"></use>
           </svg>
         </button>
       </div>
