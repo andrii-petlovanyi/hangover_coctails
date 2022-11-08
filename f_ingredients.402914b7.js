ad2282d887e78e5837e2eccadaa4e5d2fc447d03
andrii-petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-08 15:48:46 +0000

    Deploying to gh-pages from @ andrii-petlovanyi/hangover_coctails@9933e0f2b0c4a4424e19e5e7e4a6ea26b14df687 🚀

---
+++ f_ingredients.402914b7.js
@@ -0,0 +1,2 @@
+document.querySelector(".favorite__link").addEventListener("click",(()=>{document.querySelector(".favorite__bar").classList.toggle("active")})),document.querySelector(".menu__icon").addEventListener("click",(()=>{document.querySelector(".mobile__menu").classList.toggle("open"),document.body.classList.toggle("overflow")})),document.querySelector(".menu__close").addEventListener("click",(()=>{document.querySelector(".mobile__menu").classList.toggle("open"),document.body.classList.toggle("overflow")}));
+//# sourceMappingURL=f_ingredients.402914b7.js.map
