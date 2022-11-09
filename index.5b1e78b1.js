5604e8cde7cc8c830a6e0c416107d7b0eb82a7be
andrii-petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-09 15:42:00 +0000

    Deploying to gh-pages from @ andrii-petlovanyi/hangover_coctails@be7afece45d96cb9b1f47fc3bdc14c0a556b7ecb 🚀

---
+++ index.5b1e78b1.js
@@ -0,0 +1,2 @@
+function e(){document.querySelector(".hero__list").classList.toggle("active"),document.querySelector(".hero").classList.toggle("active")}document.querySelector(".hero__svg").addEventListener("click",e),document.querySelector(".hero__select").addEventListener("mouseleave",e);
+//# sourceMappingURL=index.5b1e78b1.js.map
---
+++ index.a6a20e97.js
@@ -0,0 +1,2 @@
+!function(){function e(){document.querySelector(".hero__list").classList.toggle("active"),document.querySelector(".hero").classList.toggle("active")}document.querySelector(".hero__svg").addEventListener("click",e),document.querySelector(".hero__select").addEventListener("mouseleave",e)}();
+//# sourceMappingURL=index.a6a20e97.js.map
