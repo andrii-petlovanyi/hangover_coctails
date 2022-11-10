96093c4f1094a08bbd79705f578227567eaf3269
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-10 12:03:36 +0200

    Update favorite_bar.js

--- src/js/components/header/favorite_bar.js
+++ src/js/components/header/favorite_bar.js
@@ -5,7 +5,7 @@ document.querySelector('.js-menu').addEventListener('mouseleave', () => {
 
 document.querySelector('.favorite__link').addEventListener('mouseenter', () => {
   document.querySelector('.favorite__bar').classList.add('active');
-
+});
 
 document.querySelector('.menu__icon').addEventListener('click', () => {
   document.querySelector('.mobile__menu').classList.toggle('open');
