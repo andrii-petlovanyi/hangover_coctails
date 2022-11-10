658077d9a0856721d569a3dcfeb4b56bdc057e63
Maksym <PTNPNH722@gmail.com>
2022-11-10 11:54:53 +0200

    header v1.4

--- src/js/components/header/favorite_bar.js
+++ src/js/components/header/favorite_bar.js
@@ -1,9 +1,9 @@
-document.querySelector('.favorite__link').addEventListener('click', () => {
-    document.querySelector('.favorite__bar').classList.toggle('active');
+document.querySelector('.js-menu').addEventListener('mouseleave', () => {
+  document.querySelector('.favorite__bar').classList.remove('active');
 });
 
-document.querySelector('.favorite__link').addEventListener('mouseover', () => {
-    document.querySelector('.favorite__bar').classList.toggle('active');
+document.querySelector('.favorite__link').addEventListener('mouseenter', () => {
+  document.querySelector('.favorite__bar').classList.add('active');
 });
 
 document.querySelector(".menu__icon").addEventListener("click", () => {
