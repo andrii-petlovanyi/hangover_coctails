72923ba6eae61eed0fdb4ad2a51362780b7f1533
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-10 12:53:56 +0200

    add some

--- src/js/components/favourites/index.js
+++ src/js/components/favourites/index.js
@@ -2,7 +2,7 @@ import { FAV_COCKTAIL } from './fav_cocktails';
 import sprite from '../../../images/svg/sprite.svg';
 import { notFound } from '../error';
 import { initTheme, resetTheme } from '../switcher/switcher';
-// import { searchCoctById } from '../modal';
+import { searchCoctById } from '../modal';
 
 const refCocktList = document.querySelector('.js-add_f-coctail');
 const actArr = JSON.parse(localStorage.getItem(FAV_COCKTAIL)) || [];
@@ -32,7 +32,7 @@ function deleteCard(e) {
     e.target.parentNode.parentNode.remove();
     return;
   }
-  // if (e.target.dataset.type) await searchCoctById(e.target.dataset.id);
+  if (e.target.dataset.type) await searchCoctById(e.target.dataset.id);
 }
 
 function renderMarkupList(data = []) {
