f1c8ddbc0775dd8d98be5081b832a96e87f8ddf1
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-12 17:07:35 +0200

    added some little fix

--- src/js/components/refs/index.js
+++ src/js/components/refs/index.js
@@ -1,9 +1,11 @@
-export const refFormSearch = document.querySelector('.header__input');
+export const refFormSearch = document.querySelector('.input__search');
 export const refCocktList = document.querySelector('.js-add_f-coctail');
 export const refIngrList = document.querySelector('.ingredients-list');
 export const refHeaderJSMenu = document.querySelector('.js-menu');
 export const refHeaderFavBar = document.querySelector('.header__favorite__bar');
-export const refHeaderFavLink = document.querySelector('.header__favorite__link');
+export const refHeaderFavLink = document.querySelector(
+  '.header__favorite__link'
+);
 export const refHeaderMenuOpen = document.querySelector('.menu__icon');
 export const refHeaderMenuClose = document.querySelector('.menu__close');
 export const refHeaderMobMenu = document.querySelector('.menu__mobile');
