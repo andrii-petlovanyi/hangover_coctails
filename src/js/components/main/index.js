79b16496b39c894028f5501ec911b00bb3652c27
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-13 19:45:10 +0200

    added for some element cursor pointer

--- src/js/components/main/index.js
+++ src/js/components/main/index.js
@@ -47,7 +47,7 @@ export function renderMarkup(data = []) {
           </svg>
         </button>`;
       return `<li class="coctail-card">
-      <img class="img lazyload" src="${placeholder}" data-srcset=${strDrinkThumb} alt=${strDrink}/img>
+      <img class="img lazyload" src="${placeholder}" data-srcset=${strDrinkThumb} alt=${strDrink}>
       <h3 class="coctail-card__name">${strDrink}</h3>
       <div class="coctail-card__options">
         <button class="button-learn_more" data-id=${idDrink} data-type="learn">Learn more</button>
